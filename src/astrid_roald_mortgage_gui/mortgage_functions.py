0941b66c223b70b1cce82d032632537c66bd014e
Roald Storm <roaldstorm@gmail.com>
2019-04-18 21:52:44 +0200

    Added a debt graph and an option to keep the BSUs as security

--- src/astrid_roald_mortgage_gui/mortgage_functions.py
+++ src/astrid_roald_mortgage_gui/mortgage_functions.py
@@ -30,12 +30,15 @@ def calculate_cost_while_saving(number_of_months, current_values):
 
     cumulative_cost = [0]
     time = [current_values['starting date']]
+    total_debt = [0]
     for pay_date in daterange(current_values['starting date'], number_of_months):
         if current_savings<required_savings:
             cumulative_cost.append(rent + cumulative_cost[-1])
             current_savings = current_savings + current_values['housing money'] - rent
+            total_debt.append(0)
             if current_savings >= required_savings:
                 mortgage = current_values['mortgage goal'] - current_savings
+                total_debt[-1] = mortgage
         else:
             month_cost = mortgage*current_values['mortgage monthly interest']
             cumulative_cost.append(month_cost + cumulative_cost[-1])
@@ -44,8 +47,79 @@ def calculate_cost_while_saving(number_of_months, current_values):
                 mortgage = mortgage - pay_down
             elif mortgage <= pay_down and mortgage != 0:
                 mortgage = 0
+            total_debt.append(mortgage)
         time.append(pay_date)
-    return time, cumulative_cost
+    return time, cumulative_cost, total_debt
+
+
+def do_bsu_fill(month_housing_money, bsu, bsu_left_to_fill):
+    if bsu_left_to_fill - month_housing_money > 0:
+        bsu = bsu + month_housing_money
+        bsu_left_to_fill = bsu_left_to_fill - month_housing_money
+        month_housing_money = 0
+    else:
+        bsu = bsu + bsu_left_to_fill
+        month_housing_money = month_housing_money - bsu_left_to_fill
+        bsu_left_to_fill = 0
+    return month_housing_money, bsu, bsu_left_to_fill
+
+
+def calculate_cost_while_saving_with_bsu_as_security(number_of_months, current_values):
+    rent = current_values['rent per month']
+    required_savings = current_values['mortgage goal'] / 85*15
+    bsu = current_values['bsu savings']
+    bsu2 = current_values['bsu2 savings']
+    bsu_left_to_fill = 0
+    bsu2_left_to_fill = 20000
+    current_savings = current_values['deposit'] + bsu + bsu2
+    bsu_monthly_interest = get_monthly_interest_from_yearly(3.3)
+
+    cumulative_cost = [0]
+    time = [current_values['starting date']]
+    total_debt = [0]
+    bsu_interest_this_year = (bsu + bsu2) * ((1 + bsu_monthly_interest) ** time[0].month - 1)
+    for pay_date in daterange(current_values['starting date'], number_of_months):
+        bsu_interest_this_year = bsu_interest_this_year + (bsu + bsu2) * bsu_monthly_interest
+        month_housing_money = current_values['housing money']
+        if current_savings<required_savings:
+            cumulative_cost.append(rent + cumulative_cost[-1])
+            month_savings =  month_housing_money - rent
+
+            if bsu_left_to_fill + bsu2_left_to_fill > 0:
+                if bsu_left_to_fill > 0:
+                    month_savings, bsu, bsu_left_to_fill = do_bsu_fill(month_savings, bsu, bsu_left_to_fill)
+                if bsu2_left_to_fill > 0:
+                    month_savings, bsu2, bsu2_left_to_fill = do_bsu_fill(month_savings, bsu2, bsu2_left_to_fill)
+                print(f'stillrenting: {month_savings}, {bsu}, {bsu2}')
+            current_savings = current_savings + month_savings
+            total_debt.append(0)
+            if current_savings >= required_savings:
+                mortgage = current_values['mortgage goal']
+                total_debt[-1] = mortgage
+        else:
+            month_cost = mortgage*current_values['mortgage monthly interest']
+            cumulative_cost.append(month_cost + cumulative_cost[-1])
+            pay_down = month_housing_money - month_cost
+
+            if bsu_left_to_fill + bsu2_left_to_fill > 0:
+                if bsu_left_to_fill > 0:
+                    pay_down, bsu, bsu_left_to_fill = do_bsu_fill(pay_down, bsu, bsu_left_to_fill)
+                if bsu2_left_to_fill > 0:
+                    pay_down, bsu2, bsu2_left_to_fill = do_bsu_fill(pay_down, bsu2, bsu2_left_to_fill)
+                print(f'Mortgage pay down: {pay_down}, {bsu}, {bsu2}')
+            if mortgage > pay_down:
+                mortgage = mortgage - pay_down
+            elif mortgage <= pay_down and mortgage != 0:
+                mortgage = 0
+            total_debt.append(mortgage)
+        if pay_date.month == 1:
+            cumulative_cost[-1] = cumulative_cost[-1]-bsu_interest_this_year-10000
+            print(bsu_interest_this_year)
+            bsu_interest_this_year = 0
+            bsu_left_to_fill = 50000
+            bsu2_left_to_fill = 50000
+        time.append(pay_date)
+    return time, cumulative_cost, total_debt
 
 
 def calculate_cost_with_top_loan(number_of_months, current_values):
@@ -53,6 +127,7 @@ def calculate_cost_with_top_loan(number_of_months, current_values):
     top_loan = current_values['mortgage goal']*.15 - current_values['deposit'] - \
                current_values['bsu savings'] - current_values['bsu2 savings']
     cumulative_cost = [0]
+    total_debt = [mortgage + top_loan]
     time = [current_values['starting date']]
     for pay_date in daterange(current_values['starting date'], number_of_months):
         month_cost = mortgage*current_values['mortgage monthly interest'] + \
@@ -69,7 +144,8 @@ def calculate_cost_with_top_loan(number_of_months, current_values):
         elif mortgage <= pay_down and mortgage != 0:
             mortgage = 0
         time.append(pay_date)
-    return time, cumulative_cost
+        total_debt.append(mortgage + top_loan)
+    return time, cumulative_cost, total_debt
 
 
 def main():
--- src/astrid_roald_mortgage_gui/mortgage_gui.py
+++ src/astrid_roald_mortgage_gui/mortgage_gui.py
@@ -5,7 +5,7 @@ from PySide2 import QtWidgets
 # from PySide2 import QtCore, QtGui
 
 from astrid_roald_mortgage_gui.mortgage_functions import calculate_cost_with_top_loan, calculate_cost_while_saving,\
-    get_monthly_interest_from_yearly
+    get_monthly_interest_from_yearly, calculate_cost_while_saving_with_bsu_as_security
 from astrid_roald_mortgage_gui.secrets.astrid_roald_input import astrid_roald_input
 from astrid_roald_mortgage_gui.matplotlib_widget import MyMplCanvas
 
@@ -16,21 +16,39 @@ class MortgagePlotter(QtWidgets.QDialog):
         # Set start and ending date for calculation
         self.starting_date = input_dict['date of creation']
         self.ending_date = QtWidgets.QDateEdit()
-        self.ending_date.setDate(self.starting_date+datetime.timedelta(days=360*5.5))
+        self.ending_date.setDate(self.starting_date+datetime.timedelta(days=360*7.5))
 
         # Create widgets
         self.button = QtWidgets.QPushButton("Save current cost calculation")
         self.value_widgets = {k: QtWidgets.QLineEdit(str(v)) for k, v in input_dict.items()}
-        self.mpl_canvas = MyMplCanvas(width=5, height=4, dpi=100)
-        self.cur_line = None
+        self.cost_canvas = MyMplCanvas(width=5, height=4, dpi=100)
+        self.debt_canvas = MyMplCanvas(width=5, height=4, dpi=100)
+        self.cur_lines = [None, None]
         self.use_bsu_check_box = QtWidgets.QCheckBox('Use BSU as security')
         self.use_toploan = QtWidgets.QCheckBox('Take out toploan')
 
         # Create layout and add widgets
         layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
         top_layout = QtWidgets.QHBoxLayout()
+        layout.addLayout(top_layout)
+
+        graphs_layout = QtWidgets.QVBoxLayout()
+        layout.addLayout(graphs_layout)
+
         top_left_layout = QtWidgets.QVBoxLayout()
+        top_layout.addLayout(top_left_layout)
+
         input_data_layout = QtWidgets.QFormLayout()
+        top_left_layout.addLayout(input_data_layout)
+
+        options_layout = QtWidgets.QGridLayout()
+        top_layout.addLayout(options_layout)
+
+        graphs_layout.addWidget(self.cost_canvas)
+        graphs_layout.addWidget(self.debt_canvas)
+
         for k, v in self.value_widgets.items():
             if k == 'date of creation':
                 top_left_layout.addWidget(QtWidgets.QLabel(
@@ -49,16 +67,9 @@ class MortgagePlotter(QtWidgets.QDialog):
             else:
                 raise KeyError(f'Unaccounted for key in input_dict: {k}')
 
-        options_layout = QtWidgets.QGridLayout()
         for i, widget in enumerate([self.ending_date, self.use_bsu_check_box,
                                     self.use_toploan, self.button]):
             options_layout.addWidget(widget, i, 0)
-        top_left_layout.addLayout(input_data_layout)
-        top_layout.addLayout(top_left_layout)
-        top_layout.addLayout(options_layout)
-        layout.addLayout(top_layout)
-        layout.addWidget(self.mpl_canvas)
-        self.setLayout(layout)
 
         # Connect callbacks
         self.button.clicked.connect(self.add_cost_line)
@@ -80,41 +91,59 @@ class MortgagePlotter(QtWidgets.QDialog):
                     self.value_widgets['top loan interest percentage'].text())),
                 'mortgage monthly interest': get_monthly_interest_from_yearly(float(
                     self.value_widgets['mortgage interest percentage'].text()))}
+        # 'top loan monthly interest': (1 + (float(self.value_widgets['top loan interest percentage'].text())
+        #                                    / 100)) ** (1 / 12) - 1,
+        # 'mortgage monthly interest': (1 + (float(self.value_widgets['mortgage interest percentage'].text())
+        #                                    / 100)) ** (1 / 12) - 1}
 
-    def set_cost_line_label(self):
-        self.cur_line.set_label(f"d: {self.mortgage_date.date().toString('yyyy/MM/dd')}, "
-                                f"h: {self.value_widgets['housing money'].text()}, "
-                                f"g: {self.value_widgets['mortgage goal'].text()}, "
-                                f"t%: {self.value_widgets['top loan interest percentage'].text()}, "
-                                f"m%: {self.value_widgets['mortgage interest percentage'].text()}, "
-                                f"BSU: {bool(self.use_bsu_check_box.checkState())}, "
-                                f"Top: {bool(self.use_toploan.checkState())}")
-        self.mpl_canvas.axes.legend()
+    def set_legend_labels(self):
+        label = f"d: {self.mortgage_date.date().toString('yyyy/MM/dd')}, " \
+            f"h: {self.value_widgets['housing money'].text()}, " \
+            f"g: {self.value_widgets['mortgage goal'].text()}, " \
+            f"t%: {self.value_widgets['top loan interest percentage'].text()}, " \
+            f"m%: {self.value_widgets['mortgage interest percentage'].text()}, " \
+            f"{'BSU security' if bool(self.use_bsu_check_box.checkState()) else 'BSU popped'}, " \
+            f"{'Toploan' if bool(self.use_toploan.checkState()) else ''}"
+        self.cur_lines[0].set_label(label)
+        self.cur_lines[1].set_label(label)
+        self.cost_canvas.axes.legend()
+        self.debt_canvas.axes.legend()
+
+    def redraw_canvasses(self):
+        self.set_legend_labels()
+        for canvas in [self.cost_canvas, self.debt_canvas]:
+            canvas.axes.relim()
+            canvas.axes.autoscale_view()
+            canvas.draw()
 
     def add_cost_line(self):
-        time, cost_list = self.get_cost()
-        self.mpl_canvas.axes.plot(time, cost_list)
-        self.cur_line = self.mpl_canvas.axes.lines[-1]
-        self.set_cost_line_label()
-
-        self.mpl_canvas.draw()
+        time, cost_list, total_debt = self.get_cost()
+        self.cost_canvas.axes.plot(time, cost_list)
+        self.cur_lines[0] = self.cost_canvas.axes.lines[-1]
+        self.debt_canvas.axes.plot(time, total_debt)
+        self.cur_lines[1] = self.debt_canvas.axes.lines[-1]
+        self.redraw_canvasses()
 
     def change_current_cost_line(self):
-        time, cost_list = self.get_cost()
-        self.cur_line.set_ydata(cost_list)
-        self.cur_line.set_xdata(time)
-        self.set_cost_line_label()
-        self.mpl_canvas.axes.relim()
-        self.mpl_canvas.axes.autoscale_view()
-
-        self.mpl_canvas.draw()
+        time, cost_list, total_debt = self.get_cost()
+        self.cur_lines[0].set_xdata(time)
+        self.cur_lines[0].set_ydata(cost_list)
+        self.cur_lines[1].set_xdata(time)
+        self.cur_lines[1].set_ydata(total_debt)
+        self.redraw_canvasses()
 
     def get_cost(self):
         number_of_months = round((self.ending_date.date().toPython() - self.starting_date.date()).days / 365 * 12)
-        if bool(self.use_toploan.checkState()):
-            return calculate_cost_with_top_loan(number_of_months, self.get_current_values())
+        if bool(self.use_bsu_check_box.checkState()):
+            if bool(self.use_toploan.checkState()):
+                raise RuntimeError('Method not yet implemented')
+            else:
+                return calculate_cost_while_saving_with_bsu_as_security(number_of_months, self.get_current_values())
         else:
-            return calculate_cost_while_saving(number_of_months, self.get_current_values())
+            if bool(self.use_toploan.checkState()):
+                return calculate_cost_with_top_loan(number_of_months, self.get_current_values())
+            else:
+                return calculate_cost_while_saving(number_of_months, self.get_current_values())
 
 
 def run_app():
@@ -122,7 +151,7 @@ def run_app():
     # from astrid_roald_mortgage_gui.mortgage_functions import example_input
     # widget = MortgagePlotter(example_input)
     widget = MortgagePlotter(astrid_roald_input)
-    widget.resize(1200, 900)
+    widget.resize(1200, 980)
     widget.show()
     sys.exit(app.exec_())
 
