f8e34c3713a347882be7b78b70938e8cd1ad74e2
Roald Storm <roaldstorm@gmail.com>
2019-04-18 12:06:08 +0200

    Refactored how values are passed from interface to mortage functions

--- src/astrid_roald_mortgage_gui/mortgage_functions.py
+++ src/astrid_roald_mortgage_gui/mortgage_functions.py
@@ -4,7 +4,8 @@ example_input = {'date of creation': datetime.datetime(2019, 3, 20),
                  'rent per month': 1000,
                  'housing money': 2000,
                  'deposit': 1000,
-                 'total savings': 2000,
+                 'bsu savings': 2000,
+                 'bsu2 savings': 2000,
                  'mortgage goal': 400000,
                  'top loan interest percentage': 10,
                  'mortgage interest percentage': 1}
@@ -14,15 +15,19 @@ def get_monthly_interest_from_yearly(yearly_interest_percentage: float) -> float
     return (1+(yearly_interest_percentage/100))**(1/12)-1
 
 
-def calculate_cost_with_top_loan(duration, housing_money, mortgage, top_loan,
-                                 mortgage_yearly_interest, top_loan_yearly_interest):
-    mortgage_monthly_interest = get_monthly_interest_from_yearly(mortgage_yearly_interest)
-    top_loan_monthly_interest = get_monthly_interest_from_yearly(top_loan_yearly_interest)
+def calculate_cost_while_saving(duration, housing_money, mortgage, mortgage_yearly_interest):
+    pass
+
+
+def calculate_cost_with_top_loan(duration, current_values):
+    mortgage = current_values['mortgage goal']*.85
+    top_loan = current_values['mortgage goal']*.15
     cumulative_cost = [0]
     for i in range(duration):
-        month_cost = mortgage*mortgage_monthly_interest + top_loan*top_loan_monthly_interest
+        month_cost = mortgage*current_values['mortgage monthly interest'] + \
+                     top_loan*current_values['top loan monthly interest']
         cumulative_cost.append(month_cost + cumulative_cost[-1])
-        pay_down = housing_money - month_cost
+        pay_down = current_values['housing money'] - month_cost
         if top_loan > pay_down:
             top_loan = top_loan - pay_down
         elif top_loan <= pay_down and top_loan != 0:
@@ -32,16 +37,11 @@ def calculate_cost_with_top_loan(duration, housing_money, mortgage, top_loan,
             mortgage = mortgage - pay_down
         elif mortgage <= pay_down and mortgage != 0:
             mortgage = 0
-        # else:
-        #     break
     return cumulative_cost
 
 
 def main():
-    print(calculate_cost_with_top_loan(100, example_input['housing money'],
-                                       example_input['mortgage goal'] * .85, example_input['mortgage goal'] * .15,
-                                       example_input['mortgage interest percentage'],
-                                       example_input['top loan interest percentage']))
+    pass
 
 
 if __name__ == '__main__':
--- src/astrid_roald_mortgage_gui/mortgage_gui.py
+++ src/astrid_roald_mortgage_gui/mortgage_gui.py
@@ -1,29 +1,29 @@
 import sys
-import datetime
 
-import matplotlib
-from PySide2 import QtWidgets, QtCharts
+from PySide2 import QtWidgets
 # from PySide2 import QtCore, QtGui
 
-from astrid_roald_mortgage_gui.mortgage_functions import calculate_cost_with_top_loan, example_input
+from astrid_roald_mortgage_gui.mortgage_functions import calculate_cost_with_top_loan, get_monthly_interest_from_yearly
 from astrid_roald_mortgage_gui.secrets.astrid_roald_input import astrid_roald_input
 from astrid_roald_mortgage_gui.matplotlib_widget import MyMplCanvas
 
-class Mortgage_plotter(QtWidgets.QDialog):
+
+class MortgagePlotter(QtWidgets.QDialog):
     def __init__(self, input_dict, parent=None):
-        super(Mortgage_plotter, self).__init__(parent)
+        super(MortgagePlotter, self).__init__(parent)
         # Create widgets
         self.button = QtWidgets.QPushButton("Save current cost calculation")
-        self.edit_widgets = {k: QtWidgets.QLineEdit(str(v)) for k, v in input_dict.items()}
+        self.value_widgets = {k: QtWidgets.QLineEdit(str(v)) for k, v in input_dict.items()}
         self.mpl_canvas = MyMplCanvas(width=5, height=4, dpi=100)
-        self.check_box = QtWidgets.QCheckBox('sometext')
+        self.cur_line = None
+        self.use_bsu_check_box = QtWidgets.QCheckBox('Use BSU as security')
 
         # Create layout and add widgets
         layout = QtWidgets.QVBoxLayout()
         input_layout = QtWidgets.QHBoxLayout()
         raw_data_layout = QtWidgets.QVBoxLayout()
         raw_data_fields_layout = QtWidgets.QFormLayout()
-        for k, v in self.edit_widgets.items():
+        for k, v in self.value_widgets.items():
             if k == 'date of creation':
                 print(input_dict[k])
                 raw_data_layout.addWidget(QtWidgets.QLabel(f'The starting date for the input data is {input_dict[k]}'))
@@ -31,8 +31,9 @@ class Mortgage_plotter(QtWidgets.QDialog):
                 self.date_widget.setDate(input_dict[k])
 
                 raw_data_fields_layout.addRow('mortgage date', self.date_widget)
-            elif k in ['rent per month', 'deposit', 'total savings']:
-                raw_data_layout.addWidget(QtWidgets.QLabel(f'The value for {k} at the starting date is {input_dict[k]}'))
+            elif k in ['rent per month', 'deposit', 'bsu savings', 'bsu2 savings']:
+                raw_data_layout.addWidget(QtWidgets.QLabel(
+                    f'The value for {k} at the starting date is {input_dict[k]} NOK'))
             elif k in ['mortgage goal', 'housing money',
                        'top loan interest percentage', 'mortgage interest percentage']:
                 raw_data_fields_layout.addRow(k, v)
@@ -41,12 +42,12 @@ class Mortgage_plotter(QtWidgets.QDialog):
                 raise KeyError(f'Unaccounted for key in input_dict: {k}')
 
         options_layout = QtWidgets.QGridLayout()
-        options_layout.addWidget(self.check_box, 0, 0)
+        options_layout.addWidget(self.use_bsu_check_box, 0, 0)
+        options_layout.addWidget(self.button, 1, 0)
         raw_data_layout.addLayout(raw_data_fields_layout)
         input_layout.addLayout(raw_data_layout)
         input_layout.addLayout(options_layout)
         layout.addLayout(input_layout)
-        layout.addWidget(self.button)
         layout.addWidget(self.mpl_canvas)
 
         # Set layout into widget
@@ -56,17 +57,25 @@ class Mortgage_plotter(QtWidgets.QDialog):
         self.date_widget.dateChanged.connect(self.change_current_cost_line)
         self.add_cost_line()
 
+    def get_current_values(self):
+        return {'mortgage date': self.date_widget.date().toPython(),
+                'mortgage goal': float(self.value_widgets['mortgage goal'].text()),
+                'housing money': float(self.value_widgets['housing money'].text()),
+                'top loan monthly interest': get_monthly_interest_from_yearly(float(
+                    self.value_widgets['top loan interest percentage'].text())),
+                'mortgage monthly interest': get_monthly_interest_from_yearly(float(
+                    self.value_widgets['mortgage interest percentage'].text()))}
+
     def set_cost_line_label(self):
         self.cur_line.set_label(f"d: {self.date_widget.date().toString('yyyy/MM/dd')}, "
-                                f"h: {self.edit_widgets['housing money'].text()}, "
-                                f"g: {self.edit_widgets['mortgage goal'].text()}, "
-                                f"t%: {self.edit_widgets['top loan interest percentage'].text()}, "
-                                f"m%: {self.edit_widgets['mortgage interest percentage'].text()}, ")
+                                f"h: {self.value_widgets['housing money'].text()}, "
+                                f"g: {self.value_widgets['mortgage goal'].text()}, "
+                                f"t%: {self.value_widgets['top loan interest percentage'].text()}, "
+                                f"m%: {self.value_widgets['mortgage interest percentage'].text()}, ")
         self.mpl_canvas.axes.legend()
-        pass
 
     def add_cost_line(self):
-        cost_list = self.get_cost_list()
+        cost_list = calculate_cost_with_top_loan(100, self.get_current_values())
         self.mpl_canvas.axes.plot(range(len(cost_list)), cost_list)
         self.cur_line = self.mpl_canvas.axes.lines[-1]
         self.set_cost_line_label()
@@ -74,8 +83,7 @@ class Mortgage_plotter(QtWidgets.QDialog):
         self.mpl_canvas.draw()
 
     def change_current_cost_line(self):
-        cost_list = self.get_cost_list()
-        print('setting new info')
+        cost_list = calculate_cost_with_top_loan(100, self.get_current_values())
         self.cur_line.set_ydata(cost_list)
         self.cur_line.set_xdata(range(len(cost_list)))
         self.set_cost_line_label()
@@ -85,20 +93,11 @@ class Mortgage_plotter(QtWidgets.QDialog):
         self.mpl_canvas.draw()
 
 
-    def get_cost_list(self):
-        try:
-            return calculate_cost_with_top_loan(100, float(self.edit_widgets['housing money'].text()),
-                                                float(self.edit_widgets['mortgage goal'].text()) * .85,
-                                                float(self.edit_widgets['mortgage goal'].text()) * .15,
-                                                float(self.edit_widgets['mortgage interest percentage'].text()),
-                                                float(self.edit_widgets['top loan interest percentage'].text()))
-        except Exception:
-            print('Fill in some valid values')
-
 def run_app():
     app = QtWidgets.QApplication([])
-    widget = Mortgage_plotter(example_input)
-    # widget = Mortgage_plotter(astrid_roald_input)
+    # from astrid_roald_mortgage_gui.mortgage_functions import example_input
+    # widget = MortgagePlotter(example_input)
+    widget = MortgagePlotter(astrid_roald_input)
     widget.resize(1200, 900)
     widget.show()
     sys.exit(app.exec_())
