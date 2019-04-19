d9cc615282cd45fc8a0df3edb0c2196867f35bd2
Roald Storm <roaldstorm@gmail.com>
2019-04-19 22:25:30 +0200

    Added in seperate economies for partners

--- src/astrid_roald_mortgage_gui/mortgage_functions.py
+++ src/astrid_roald_mortgage_gui/mortgage_functions.py
@@ -1,14 +1,34 @@
+import copy
 import datetime
 
-example_input = {'date of creation': datetime.datetime(2019, 3, 20),
-                 'rent per month': 1000,
-                 'housing money': 2000,
-                 'deposit': 1000,
-                 'bsu savings': 2000,
-                 'bsu2 savings': 2000,
-                 'mortgage goal': 400000,
-                 'top loan interest percentage': 10,
-                 'mortgage interest percentage': 1}
+
+class Person():
+    def __init__(self, birth_date, name, housing_money, bsu, bsu2, bsu_left_to_fill, bsu2_left_to_fill):
+        self.birth_date = birth_date
+        self.name = name
+        self.housing_money = housing_money
+        self.bsu = bsu
+        self.bsu2 = bsu2
+        self.bsu_left_to_fill = bsu_left_to_fill
+        self.bsu2_left_to_fill = bsu2_left_to_fill
+
+
+class AnalysisStartValues():
+    def __init__(self, persons, simulation_start_date, rent,
+                 mortgage_goal, top_loan_interest_percentage, mortgage_interest_percentage):
+        self.persons = persons
+        self.simulation_start_date = simulation_start_date
+        self.rent = rent
+        self.deposit = 3*rent
+        self.bsu_interest_percentage = 3.6
+        self.mortgage_goal = mortgage_goal
+        self.top_loan_interest_percentage = top_loan_interest_percentage
+        self.mortgage_interest_percentage = mortgage_interest_percentage
+
+
+example_input = AnalysisStartValues([Person(datetime.date(1990, 1, 1), 'p1', 1000, 1000, 500, 1000, 1500),
+                                     Person(datetime.date(1990, 1, 1), 'p2', 1200, 1000, 1000, 0, 0)],
+                                    datetime.date(2019, 3, 20), 1000, 200000, 10, 1)
 
 
 def get_monthly_interest_from_yearly(yearly_interest_percentage: float) -> float:
@@ -16,84 +36,52 @@ def get_monthly_interest_from_yearly(yearly_interest_percentage: float) -> float
 
 
 class SavingsSimulation():
-    def __init__(self, sim_values):
-        self.mortgage_goal =sim_values['mortgage goal']
+    def __init__(self, analysis_variables, analysis_start_values: AnalysisStartValues):
+        self.mortgage_goal = analysis_variables.mortgage_goal
         self.mortgage = 0
         self.top_loan = 0
 
-        self.rent = sim_values['rent per month']
-        self.regular_savings = sim_values['deposit']
+        self.rent = analysis_start_values.rent
+        self.regular_savings = analysis_start_values.deposit
 
-        self.bsu = sim_values['bsu savings']
-        self.bsu2 = sim_values['bsu2 savings']
+        self.persons = {person.name: copy.deepcopy(person) for person in analysis_start_values.persons}
+        for name, person in self.persons.items():
+            person.housing_money = float(analysis_variables.housing_money_widgets[name].text())
+        self.total_housing_money = analysis_variables.total_housing_money
 
-        self.bsu_left_to_fill = 0
-        self.bsu2_left_to_fill = 20000
+        self.bsu_interest = get_monthly_interest_from_yearly(analysis_start_values.bsu_interest_percentage)
+        self.mortgage_interest = get_monthly_interest_from_yearly(analysis_variables.mortgage_interest_percentage)
+        self.top_loan_interest = get_monthly_interest_from_yearly(analysis_variables.top_loan_interest_percentage)
 
-        # These three interest values are all per month
-        self.bsu_interest = get_monthly_interest_from_yearly(3.6)
-        self.mortgage_interest = sim_values['mortgage monthly interest']
-        self.top_loan_interest = sim_values['top loan monthly interest']
+    def get_total_bsu_value(self):
+        return sum(person.bsu + person.bsu2 for person in self.persons.values())
 
-    def start_mortgage(self, use_bsu_as_security):
-        if use_bsu_as_security:
-            security = self.mortgage_goal + self.bsu + self.bsu2
-            self.mortgage = min(security * .85, self.mortgage_goal - self.regular_savings)
-            self.top_loan = max(self.mortgage_goal - self.regular_savings - security * .85, 0)
-            self.regular_savings = 0
-        else:
-            security = self.mortgage_goal
-            savings = self.empty_savings()
-            self.mortgage = min(security * .85, self.mortgage_goal - savings)
-            self.top_loan = max(self.mortgage_goal - savings - security * .85, 0)
-
-    def get_total_savings(self):
-        return self.bsu + self.bsu2 + self.regular_savings
-
-    def get_month_cost(self, got_mortgage):
-        if got_mortgage:
-            return self.mortgage * self.mortgage_interest + self.top_loan * self.top_loan_interest
-        else:
-            return self.rent
-
-    def pay_down_debt(self, month_leftover_money):
-        if self.top_loan > month_leftover_money:
-            self.top_loan -= month_leftover_money
-        elif self.top_loan <= month_leftover_money and self.top_loan != 0:
-            self.mortgage += self.top_loan - month_leftover_money
-            self.top_loan = 0
-        elif self.mortgage > month_leftover_money:
-            self.mortgage -= month_leftover_money
-        elif self.mortgage <= month_leftover_money and self.mortgage != 0:
-            self.mortgage = 0
-        else:
-            self.regular_savings += month_leftover_money
-
-    def new_bsu_year(self):
-        self.bsu_left_to_fill = 50000
-        self.bsu2_left_to_fill = 50000
-
-    def empty_savings(self):
-        total_savings = self.get_total_savings()
-        self.bsu = 0
-        self.bsu2 = 0
-        self.regular_savings = 0
-        return total_savings
-
-    def get_bsu_interest_for_one_month(self):
-        return (self.bsu + self.bsu2) * self.bsu_interest
+    def get_bsu_interest_for_one_month(self, interest_so_far):
+        return interest_so_far + ((self.get_total_bsu_value() + interest_so_far) * self.bsu_interest)
 
     def get_bsu_interest_for_several_months(self, months):
-        return (self.bsu + self.bsu2) * ((1 + self.bsu_interest) ** months - 1)
+        return self.get_total_bsu_value() * ((1 + self.bsu_interest) ** months - 1)
 
-    def top_up_bsus(self, month_leftover_money):
-        if self.bsu_left_to_fill > 0:
-            month_leftover_money, self.bsu, self.bsu_left_to_fill = \
-                self.do_bsu_fill(month_leftover_money, self.bsu, self.bsu_left_to_fill)
-        if self.bsu2_left_to_fill > 0:
-            month_leftover_money, self.bsu2, self.bsu2_left_to_fill = \
-                self.do_bsu_fill(month_leftover_money, self.bsu2, self.bsu2_left_to_fill)
-        return month_leftover_money
+    def get_month_cost(self, got_mortgage, this_months_money):
+        if got_mortgage:
+            interest_cost = self.mortgage * self.mortgage_interest + self.top_loan * self.top_loan_interest
+            for name, month_money in list(this_months_money.items()):
+                this_months_money[name] = month_money - interest_cost * month_money / self.total_housing_money
+            return interest_cost, this_months_money
+        else:
+            for name, month_money in list(this_months_money.items()):
+                this_months_money[name] = month_money - self.rent / len(this_months_money)
+            return self.rent, this_months_money
+
+    def top_up_bsus(self, this_months_money):
+        for name, month_money in list(this_months_money.items()):
+            if self.persons[name].bsu_left_to_fill > 0:
+                this_months_money[name], self.persons[name].bsu, self.persons[name].bsu_left_to_fill = \
+                    self.do_bsu_fill(month_money, self.persons[name].bsu, self.persons[name].bsu_left_to_fill)
+            if self.persons[name].bsu2_left_to_fill > 0:
+                this_months_money[name], self.persons[name].bsu2, self.persons[name].bsu2_left_to_fill = \
+                    self.do_bsu_fill(month_money, self.persons[name].bsu2, self.persons[name].bsu2_left_to_fill)
+        return this_months_money
 
     @staticmethod
     def do_bsu_fill(month_leftover_money, bsu, bsu_left_to_fill):
@@ -107,6 +95,47 @@ class SavingsSimulation():
             bsu_left_to_fill = 0
         return month_leftover_money, bsu, bsu_left_to_fill
 
+    def empty_savings(self):
+        total_savings = self.get_total_bsu_value() + self.regular_savings
+        for name in list(self.persons.keys()):
+            self.persons[name].bsu2 = 0
+            self.persons[name].bsu = 0
+        self.regular_savings = 0
+        return total_savings
+
+    def start_mortgage(self, use_bsu_as_security):
+        if use_bsu_as_security:
+            security = self.mortgage_goal + self.get_total_bsu_value()
+            self.mortgage = min(security * .85, self.mortgage_goal - self.regular_savings)
+            self.top_loan = max(self.mortgage_goal - self.regular_savings - security * .85, 0)
+            self.regular_savings = 0
+        else:
+            security = self.mortgage_goal
+            savings = self.empty_savings()
+            self.mortgage = min(security * .85, self.mortgage_goal - savings)
+            self.top_loan = max(self.mortgage_goal - savings - security * .85, 0)
+
+    def pay_down_debt(self, this_months_money):
+        combined_money = sum(this_months_money.values())
+        if self.top_loan > combined_money:
+            self.top_loan -= combined_money
+        elif self.top_loan <= combined_money and self.top_loan != 0:
+            self.mortgage += self.top_loan - combined_money
+            self.top_loan = 0
+        elif self.mortgage > combined_money:
+            self.mortgage -= combined_money
+        elif self.mortgage <= combined_money and self.mortgage != 0:
+            self.mortgage = 0
+        else:
+            self.regular_savings += combined_money
+
+    def new_bsu_year(self):
+        bsu_tax_rebate = sum(25000 - person.bsu_left_to_fill for person in self.persons.values() if person.bsu > 0)
+        for name in list(self.persons.keys()):
+            self.persons[name].bsu_left_to_fill = 25000
+            self.persons[name].bsu2_left_to_fill = 25000
+        return bsu_tax_rebate * .2
+
 
 def date_range(start_date, months):
     if start_date.day > 28:
@@ -116,39 +145,39 @@ def date_range(start_date, months):
                                 (start_date.month + n - 1) % 12 + 1, start_date.day)
 
 
-def calculate_cost(number_of_months, current_values, use_bsu_as_security):
-    saving_simulation = SavingsSimulation(current_values)
-    cumulative_cost, total_debt, time = [0], [0], [current_values['starting date']]
+def calculate_cost(number_of_months, analysis_variables, analysis_start_values: AnalysisStartValues):
+    saving_simulation = SavingsSimulation(analysis_variables, analysis_start_values)
+    cumulative_cost, total_debt, time = [0], [0], [analysis_start_values.simulation_start_date]
     bsu_interest_this_year = saving_simulation.get_bsu_interest_for_several_months(time[0].month)
-    got_mortgage = False
+    started_mortgage = False
 
-    for pay_date in date_range(current_values['starting date'], number_of_months):
-        time.append(pay_date)
-        this_months_cost = saving_simulation.get_month_cost(got_mortgage)
-        cumulative_cost.append(this_months_cost + cumulative_cost[-1])
-        month_leftover_money = current_values['housing money']-this_months_cost
-
-        # If saving, save up the money in the regular savings, otherwise pay down mortgage
-        if pay_date < current_values['mortgage date']:
-            month_leftover_money = saving_simulation.top_up_bsus(month_leftover_money)
-            saving_simulation.regular_savings += month_leftover_money
-            total_debt.append(0)
-        else:
-            if not got_mortgage:
-                saving_simulation.start_mortgage(use_bsu_as_security)
-                got_mortgage = True
-                top_loan = saving_simulation.top_loan
-            if use_bsu_as_security:
-                month_leftover_money = saving_simulation.top_up_bsus(month_leftover_money)
-            saving_simulation.pay_down_debt(month_leftover_money)
-            total_debt.append(saving_simulation.mortgage + saving_simulation.top_loan)
-
-        bsu_interest_this_year += saving_simulation.get_bsu_interest_for_one_month()
+    for pay_date in date_range(analysis_start_values.simulation_start_date, number_of_months):
         if pay_date.month == 1:
-            bsu_tax_rebate = (50000-saving_simulation.bsu_left_to_fill)*.2 if saving_simulation.bsu != 0 else 0
+            bsu_tax_rebate = saving_simulation.new_bsu_year()
             cumulative_cost[-1] = cumulative_cost[-1]-bsu_interest_this_year - bsu_tax_rebate
             bsu_interest_this_year = 0
-            saving_simulation.new_bsu_year()
+        else:
+            bsu_interest_this_year = saving_simulation.get_bsu_interest_for_one_month(bsu_interest_this_year)
+        this_months_money = {name: person.housing_money for name, person in saving_simulation.persons.items()}
+        time.append(pay_date)
+
+        this_months_cost, this_months_money = saving_simulation.get_month_cost(started_mortgage, this_months_money)
+        cumulative_cost.append(this_months_cost + cumulative_cost[-1])
+
+        # If saving, save up the money in the regular savings, otherwise pay down mortgage
+        if pay_date < analysis_variables.mortgage_date_widget.date().toPython():
+            this_months_money = saving_simulation.top_up_bsus(this_months_money)
+            saving_simulation.regular_savings += sum(this_months_money.values())
+            total_debt.append(0)
+        else:
+            if not started_mortgage:
+                saving_simulation.start_mortgage(analysis_variables.use_bsu_as_security)
+                started_mortgage = True
+                top_loan = saving_simulation.top_loan
+            if analysis_variables.use_bsu_as_security:
+                this_months_money = saving_simulation.top_up_bsus(this_months_money)
+            saving_simulation.pay_down_debt(this_months_money)
+            total_debt.append(saving_simulation.mortgage + saving_simulation.top_loan)
     return time, cumulative_cost, total_debt, top_loan
 
 
--- src/astrid_roald_mortgage_gui/mortgage_gui.py
+++ src/astrid_roald_mortgage_gui/mortgage_gui.py
@@ -4,28 +4,71 @@ import datetime
 from PySide2 import QtWidgets
 # from PySide2 import QtCore, QtGui
 
-from astrid_roald_mortgage_gui.mortgage_functions import calculate_cost, get_monthly_interest_from_yearly
+from astrid_roald_mortgage_gui.mortgage_functions import calculate_cost, AnalysisStartValues
 from astrid_roald_mortgage_gui.secrets.astrid_roald_input import astrid_roald_input
 from astrid_roald_mortgage_gui.matplotlib_widget import MyMplCanvas
 
 
+class AnalysisVariables():
+    def __init__(self, analysis_start_values: AnalysisStartValues, callback, input_data_layout,
+                 use_bsu_as_security_widget, mortgage_date_widget):
+        self.housing_money_widgets = {}
+
+        self.use_bsu_as_security_widget = use_bsu_as_security_widget
+        self.use_bsu_as_security_widget.stateChanged.connect(callback)
+        self.mortgage_date_widget = mortgage_date_widget
+        self.mortgage_date_widget.setDate(analysis_start_values.simulation_start_date + datetime.timedelta(days=73))
+        self.mortgage_date_widget.dateChanged.connect(callback)
+        input_data_layout.addRow('mortgage date', self.mortgage_date_widget)
+
+        for person in analysis_start_values.persons:
+            widget = QtWidgets.QLineEdit(str(person.housing_money))
+            widget.textChanged.connect(callback)
+            self.housing_money_widgets[person.name] = widget
+            input_data_layout.addRow(f'Housing money {person.name}', widget)
+
+        for field in ['mortgage_goal', 'top_loan_interest_percentage', 'mortgage_interest_percentage']:
+            widget = QtWidgets.QLineEdit(str(getattr(analysis_start_values, field)))
+            setattr(self, f'{field}_widget', widget)
+            widget.textChanged.connect(callback)
+            input_data_layout.addRow(field.replace('_', ' '), getattr(self, f'{field}_widget'))
+
+    @property
+    def total_housing_money(self):
+        return sum(float(widget.text()) for widget in self.housing_money_widgets.values())
+
+    @property
+    def use_bsu_as_security(self):
+        return bool(self.use_bsu_as_security_widget.checkState())
+
+    @property
+    def mortgage_date(self):
+        return self.mortgage_date_widget.date().toPython()
+
+    @property
+    def mortgage_goal(self):
+        return float(self.mortgage_goal_widget.text())
+
+    @property
+    def top_loan_interest_percentage(self):
+        return float(self.top_loan_interest_percentage_widget.text())
+
+    @property
+    def mortgage_interest_percentage(self):
+        return float(self.mortgage_interest_percentage_widget.text())
+
+
 class MortgagePlotter(QtWidgets.QDialog):
-    def __init__(self, input_dict, parent=None):
+    def __init__(self, analysis_start_values: AnalysisStartValues, parent=None):
         super(MortgagePlotter, self).__init__(parent)
         # Set start and ending date for calculation
-        self.starting_date = input_dict['date of creation']
+        self.analysis_start_values = analysis_start_values
+        self.starting_date = self.analysis_start_values.simulation_start_date
         self.ending_date = QtWidgets.QDateEdit()
-        self.ending_date.setDate(self.starting_date+datetime.timedelta(days=360*7.5))
+        self.ending_date.setDate(self.starting_date + datetime.timedelta(days=360*7.5))
+        self.ending_date.dateChanged.connect(self.change_current_cost_line)
 
-        # Create widgets
-        self.button = QtWidgets.QPushButton("Save current cost calculation")
-        self.value_widgets = {k: QtWidgets.QLineEdit(str(v)) for k, v in input_dict.items()}
-        self.cost_canvas = MyMplCanvas(width=5, height=4, dpi=100)
-        self.debt_canvas = MyMplCanvas(width=5, height=4, dpi=100)
-        self.cur_lines = [None, None]
-        self.use_bsu_as_security = QtWidgets.QCheckBox('Use BSU as security')
-
-        # Create layout and add widgets
+        # Create layout
         layout = QtWidgets.QVBoxLayout()
         self.setLayout(layout)
 
@@ -35,67 +78,48 @@ class MortgagePlotter(QtWidgets.QDialog):
         graphs_layout = QtWidgets.QVBoxLayout()
         layout.addLayout(graphs_layout)
 
-        top_left_layout = QtWidgets.QVBoxLayout()
-        top_layout.addLayout(top_left_layout)
+        static_info_layout = QtWidgets.QVBoxLayout()
+        top_layout.addLayout(static_info_layout)
 
         input_data_layout = QtWidgets.QFormLayout()
 
         options_layout = QtWidgets.QGridLayout()
         top_layout.addLayout(options_layout)
 
+        # Create widgets and add then to layout
+        self.analysis_variables = AnalysisVariables(analysis_start_values, self.change_current_cost_line, input_data_layout,
+                                                    QtWidgets.QCheckBox('Use BSU as security'), QtWidgets.QDateEdit())
+        self.button = QtWidgets.QPushButton("Save current cost calculation")
+        self.button.clicked.connect(self.add_cost_line)
+
+        self.cost_canvas = MyMplCanvas(width=5, height=4, dpi=100)
+        self.debt_canvas = MyMplCanvas(width=5, height=4, dpi=100)
         graphs_layout.addWidget(self.cost_canvas)
         graphs_layout.addWidget(self.debt_canvas)
 
-        for k, v in self.value_widgets.items():
-            if k == 'date of creation':
-                top_left_layout.addWidget(QtWidgets.QLabel(
-                    f'The starting date for the input data is {self.starting_date}'))
-                self.mortgage_date = QtWidgets.QDateEdit()
-                self.mortgage_date.setDate(input_dict[k]+datetime.timedelta(days=65))
+        # Add static info
+        bsu_total = sum(person.bsu + person.bsu2 for person in analysis_start_values.persons)
+        info_labels = [f'The starting date for the input data is {self.analysis_start_values.simulation_start_date}',
+                       f'The value for rent at the starting date is {analysis_start_values.rent} NOK',
+                       f'The value for deposit at the starting date is {analysis_start_values.deposit} NOK',
+                       f'The total bsu value at the starting date is {bsu_total} NOK']
+        [static_info_layout.addWidget(QtWidgets.QLabel(info_label)) for info_label in info_labels]
+        static_info_layout.addLayout(input_data_layout)
 
-                input_data_layout.addRow('mortgage date', self.mortgage_date)
-            elif k in ['rent per month', 'deposit', 'bsu savings', 'bsu2 savings']:
-                top_left_layout.addWidget(QtWidgets.QLabel(
-                    f'The value for {k} at the starting date is {input_dict[k]} NOK'))
-            elif k in ['mortgage goal', 'housing money',
-                       'top loan interest percentage', 'mortgage interest percentage']:
-                input_data_layout.addRow(k, v)
-                v.textChanged.connect(self.change_current_cost_line)
-            else:
-                raise KeyError(f'Unaccounted for key in input_dict: {k}')
-        top_left_layout.addLayout(input_data_layout)
-
-        for i, widget in enumerate([self.ending_date, self.use_bsu_as_security, self.button]):
+        for i, widget in enumerate([self.ending_date, self.analysis_variables.use_bsu_as_security_widget, self.button]):
             options_layout.addWidget(widget, i, 0)
 
-        # Connect callbacks
-        self.button.clicked.connect(self.add_cost_line)
-        for callback in [self.mortgage_date.dateChanged, self.ending_date.dateChanged,
-                         self.use_bsu_as_security.stateChanged]:
-            callback.connect(self.change_current_cost_line)
+        # Do first simulation
+        self.cur_lines = [None, None]
         self.add_cost_line()
 
-    def get_current_values(self):
-        return {'starting date': self.starting_date,
-                'rent per month': float(self.value_widgets['rent per month'].text()),
-                'deposit': float(self.value_widgets['deposit'].text()),
-                'bsu savings': float(self.value_widgets['bsu savings'].text()),
-                'bsu2 savings': float(self.value_widgets['bsu2 savings'].text()),
-                'mortgage date': self.mortgage_date.date().toPython(),
-                'mortgage goal': float(self.value_widgets['mortgage goal'].text()),
-                'housing money': float(self.value_widgets['housing money'].text()),
-                'top loan monthly interest': get_monthly_interest_from_yearly(float(
-                    self.value_widgets['top loan interest percentage'].text())),
-                'mortgage monthly interest': get_monthly_interest_from_yearly(float(
-                    self.value_widgets['mortgage interest percentage'].text()))}
-
     def set_legend_labels(self, top_loan):
-        label = f"d: {self.mortgage_date.date().toString('yyyy/MM/dd')}, " \
-            f"h: {self.value_widgets['housing money'].text()}, " \
-            f"g: {self.value_widgets['mortgage goal'].text()}, " \
-            f"t%: {self.value_widgets['top loan interest percentage'].text()}, " \
-            f"m%: {self.value_widgets['mortgage interest percentage'].text()}, " \
-            f"{'BSU security' if bool(self.use_bsu_as_security.checkState()) else 'BSU popped'}, " \
+        label = f"d: {self.analysis_variables.mortgage_date.strftime('%d/%m/%Y')}, " \
+            f"h: {self.analysis_variables.total_housing_money}, " \
+            f"g: {self.analysis_variables.mortgage_goal}, " \
+            f"t%: {self.analysis_variables.top_loan_interest_percentage}, " \
+            f"m%: {self.analysis_variables.mortgage_interest_percentage}, " \
+            f"{'BSU security' if self.analysis_variables.use_bsu_as_security else 'BSU popped'}, " \
             f"Toploan: {top_loan}"
         self.cur_lines[0].set_label(label)
         self.cur_lines[1].set_label(label)
@@ -127,8 +151,9 @@ class MortgagePlotter(QtWidgets.QDialog):
         self.redraw_canvasses()
 
     def get_cost(self):
-        number_of_months = round((self.ending_date.date().toPython() - self.starting_date.date()).days / 365 * 12)
-        return calculate_cost(number_of_months, self.get_current_values(), bool(self.use_bsu_as_security.checkState()))
+        # return [0,1], [1,2], [3,1], 100
+        number_of_months = round((self.ending_date.date().toPython() - self.starting_date).days / 365 * 12)
+        return calculate_cost(number_of_months, self.analysis_variables, self.analysis_start_values)
 
 
 def run_app():
